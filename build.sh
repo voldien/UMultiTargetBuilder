6992bf0005fd36f236615a383854184c5d7f80d9
voldie <voldiekami@gmail.com>
2020-05-05 12:55:33 +0200

    Added some files.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+$unityPath=$1 # First argument - Path to the Unity executable. 
+$path=$2      # Second argument - Path to the project root direcrory.
+$unityPath -projectPath "$path" -batchmode -nographics -executeMethod Builder.PerformDefaultBuild -quit
