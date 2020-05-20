2d70590e773a6a013a0c86c30af054af70c5336c
voldie <voldiekami@gmail.com>
2020-05-20 13:58:40 +0200

    Resolved class method name.

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 $unityPath=$1 # First argument - Path to the Unity executable. 
 $path=$2      # Second argument - Path to the project root direcrory.
-$unityPath -projectPath "$path" -batchmode -nographics -executeMethod Builder.PerformDefaultBuild -quit
+$unityPath -projectPath "$path" -batchmode -nographics -executeMethod Builder.PerformBuildContext -quit
+
+
+#$unityPath -projectPath "$path" -batchmode -nographics -executeMethod Builder.PerformBuildScriptOnlyContext -quit
+
