4bf6f4a9bab26ee7b615d7c624e6de64833e2f23
voldie <voldiekami@gmail.com>
2020-05-25 16:53:21 +0200

    Improved build script and minor package.

--- build.sh
+++ build.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+#
+# SCRIPT: Building Unity target using the MultiTarget Build tool.
+# AUTHOR: Valdemar Lindberg
+# DATE: 25/05/2020
+# REV: 1.0
+#
+# PLATFORM: Not platform dependent.
+#
+# PURPOSE:# Creating Unity builds from the command line without
+# using the GUI interface.
+#
+# set -n
+# Uncomment to check script syntax, without execution.
+#
+# set -x
+# Uncomment to debug this shell script
+# 
+# Example: ./build.sh /path/unity/Editor/Unity /path/project-directory -f
+# This will build a project with the 
+#
 
-$unityPath=$1 # First argument - Path to the Unity executable. 
-$path=$2      # Second argument - Path to the project root direcrory.
-$fast=$3      # Script build or full build. ( argument s for script build and argument f for full build)
+$unityPath=$1   # First argument - Path to the Unity executable. 
+$path=$2        # Second argument - Path to the project root direcrory.
+$buildmode=$3   # Script build or full build. ( argument s for script build and argument f for full build)
+$additional=${4:-""}
 
-if [ $fast == 'f' ]
+if [ $buildmode == 'f' ]
 then
-    $unityPath -projectPath "$path" -batchmode -nographics -executeMethod BuildMultiPlatform.Builder.PerformBuildContext -quit
-elif [ $fast == 's' ]
-    $unityPath -projectPath "$path" -batchmode -nographics -executeMethod BuildMultiPlatform.Builder.PerformBuildScriptOnlyContext -quit
+    $unityPath -projectPath "$path" -batchmode -nographics -executeMethod BuildMultiPlatform.Builder.PerformBuildContext -quit $additional
+elif [ $fasbuildmodet == 's' ]
+    $unityPath -projectPath "$path" -batchmode -nographics -executeMethod BuildMultiPlatform.Builder.PerformBuildScriptOnlyContext -quit $additional
 fi
